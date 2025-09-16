0343a2048f8627f709fd443426601cce5dd6275d
ali1059 <ali6121.work@gmail.com>
2025-09-16 17:27:23 +0500

    updated config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,6 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [
-    svelte(),
-    dts({
-      insertTypesEntry: true, // generate index.d.ts
-    }),
-  ],
-  build: {
-    lib: {
-      entry: 'src/lib/UnlayerEditor.svelte', // path to your main component
-      name: 'SvelteUnlayerEditor',
-      formats: ['es', 'umd'], 
-      fileName: (format) => `unlayer-editor.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['svelte'], 
-      output: {
-        globals: {
-          svelte: 'Svelte',
-        },
-      },
-    },
-  },
-});
+  plugins: [svelte()],
+})
